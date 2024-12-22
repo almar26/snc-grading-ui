0abdf1e464c2175db783b1d1c03213cefabf3c6f
almar <almar.gomez@scribblecubes.ph>
2024-12-22 22:37:23 +0800

    add function to update faculty details

---
+++ server/api/teacher-account/update/[teacherid].ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const BASE_URL = process.env.BASE_URL
+
+export default defineEventHandler(async (event) => {
+  const teacherid = getRouterParam(event, 'teacherid')
+  const cookies = parseCookies(event);
+  const token = cookies?.token
+  try {
+    const body = await readBody(event);
+    const myPayload = {
+      faculty_no: body.faculty_no,
+      last_name: body.last_name,
+      first_name: body.first_name,
+      middle_name: body.middle_name,
+      email: body.email,
+      gender: body.gender
+    }
+    const result = await axios.put(`${BASE_URL}/api/teacher-account/update/${teacherid}`, myPayload, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    if (result) {
+      return result.data;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})
